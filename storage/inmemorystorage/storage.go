908d49f51c0a2ac0706e26c33080671fc76b4943
Andrey Minin <ewkjf@mail.ru>
2022-01-23 14:19:38 +0300

    slightly edited the inmemory storage realization - provided the adaptation to future changes.

--- storage/inmemorystorage/storage.go
+++ storage/inmemorystorage/storage.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"sync"
 	"twitter/storage"
 	"twitter/storage/generator"
@@ -19,11 +20,11 @@ type InmemoryDataSource struct {
 
 func (ids *InmemoryDataSource) Save(ctx context.Context, data storage.PostData) error {
 	for attempt := 0; attempt < 5; attempt++ {
-		_, ok := ids.IdToPost[data.Id]
+		_, ok := ids.IdToPost[data.Id.String()]
 		if ok {
 			continue
 		} else {
-			ids.IdToPost[data.Id] = data
+			ids.IdToPost[data.Id.String()] = data
 			val, _ := ids.UserIdToPosts[data.AuthorId]
 			val = append(val, data)
 			ids.UserIdToPosts[data.AuthorId] = val
@@ -47,37 +48,41 @@ func (ids *InmemoryDataSource) GetPostsByUserId(ctx context.Context, userId stri
 	if pageId == "" {
 		if ok {
 			if len(val) <= pageSize {
-				return storage.PostsByUser{val, ""}, nil
+				return storage.PostsByUser{Posts: val}, nil
 			} else {
 				newPageId := generator.GetRandomKey()
 				ids.PageIdToPageSize[newPageId] = pageSize
 				ids.PageIdToOffset[newPageId] = pageSize
-				return storage.PostsByUser{val[:pageSize], newPageId}, nil
+				objectId, err := primitive.ObjectIDFromHex(newPageId)
+				if err != nil {
+					return storage.PostsByUser{}, fmt.Errorf("invalid id - %w", storage.StorageError)
+				}
+				return storage.PostsByUser{Posts: val[:pageSize], NextPageId: objectId}, nil
 			}
 		} else {
-			return storage.PostsByUser{[]storage.PostData{}, ""}, nil
+			return storage.PostsByUser{Posts: []storage.PostData{}}, nil
 		}
 	} else {
 		if ok {
 			oldSize, ok2 := ids.PageIdToPageSize[pageId]
 			if ok2 {
 				if oldSize != pageSize {
-					return storage.PostsByUser{[]storage.PostData{}, ""}, errors.New("provided pageid is not equal to current pageid")
+					return storage.PostsByUser{Posts: []storage.PostData{}}, errors.New("provided pageid is not equal to current pageid")
 				}
 				val = val[ids.PageIdToOffset[pageId]:]
 				if len(val) <= pageSize {
-					return storage.PostsByUser{val, ""}, nil
+					return storage.PostsByUser{Posts: val}, nil
 				} else {
 					newPageId := generator.GetRandomKey()
 					ids.PageIdToPageSize[newPageId] = pageSize
 					ids.PageIdToOffset[newPageId] = ids.PageIdToOffset[pageId] + pageSize
-					return storage.PostsByUser{val[:pageSize], newPageId}, nil
+					return storage.PostsByUser{Posts: val[:pageSize]}, nil
 				}
 			} else {
-				return storage.PostsByUser{[]storage.PostData{}, ""}, errors.New("page was not found")
+				return storage.PostsByUser{Posts: []storage.PostData{}}, errors.New("page was not found")
 			}
 		} else {
-			return storage.PostsByUser{[]storage.PostData{}, ""}, errors.New("this user has no posts yet")
+			return storage.PostsByUser{Posts: []storage.PostData{}}, errors.New("this user has no posts yet")
 		}
 	}
 }
